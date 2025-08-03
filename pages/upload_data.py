0483b0b73d3eb59148ef027e4e7d33929a9a38f6
Andhika Oktasandira <114379148+Andika1710@users.noreply.github.com>
2025-08-04 05:13:08 +0700

    Update upload_data.py

--- pages/upload_data.py
+++ pages/upload_data.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import folium
-from streamlit_folium import st_folium
+
 
 def show():
     st.subheader("📁 Upload Dataset Penggunaan Listrik")
@@ -24,4 +24,5 @@ def show():
 
         pass
         
+
 
